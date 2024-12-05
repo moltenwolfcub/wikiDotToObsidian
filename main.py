75d602767c27f6591de6d7fdad1ddc02a0cb624c
MoltenWolfCub <oliver@tomlins.org.uk>
2024-12-05 20:27:28 +0000

    refactored existing code and cleaned up output

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ def extractData(html: str):
 		if "class" in section.attrs and section.attrs["class"][0] == "content-separator":
 			continue
 
+		text = section.get_text()
+
 		match i:
 			case 1:
-				text: str = section.get_text()
-
 				if text.count("Source") != 1:
 					htmlErr(i)
 
@@ -43,35 +43,35 @@ def extractData(html: str):
 				data["source"] = source
 				continue
 			case 2:
-				spellType: str = section.get_text()
-
-				data["spellType"] = spellType
+				data["spellType"] = text
 
 				# cantrips are written as <School> Cantrip
 				# other spells are written as <Level> <School>
-				if spellType.split(" ")[1].lower() == "cantrip":
+				if text.split(" ")[1].lower() == "cantrip":
 					data["level"] = 0
 				else:
-					level: str = spellType[0]
+					level: str = text[0]
 					if not level.isnumeric():
 						htmlErr(i)
 
 					data["level"] = int(level)
 				continue
 			case 3:
-				text: str = section.get_text()
-
 				if text.lower().count("casting time") != 1:
 					htmlErr(i)
 				
 				data["stats"] = text
 				continue
 		
+		# last item (excluding separators) is always spell lists
 		if i == len(mainContent)-2:
-			text = section.get_text()
 			if text.lower().count("spell lists") != 1:
 				htmlErr(i)
-			spellLists: list[str] = section.get_text().lower().split(" ")[2:]
+
+			spellLists: list[str] = section.get_text().lower().split(".")[1].split(",")
+			for i in range(len(spellLists)):
+				spellLists[i] = spellLists[i].strip()
+			
 			data["spellLists"] = spellLists
 			continue
 
@@ -88,7 +88,9 @@ def main():
 	html = getHTML("https://dnd5e.wikidot.com/spell:thunderwave")
 	data = extractData(html)
 
-	print(data)
+	print("")
+	for i in data.keys():
+		print(f"\033[94m{i}\033[0m: {data[i]}")
 
 if __name__ == '__main__':
 	main()
