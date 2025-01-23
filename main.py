09a17023d98da98b07328ef1ad2d2673e4f344b9
MoltenWolfCub <oliver@tomlins.org.uk>
2025-01-23 19:46:21 +0000

    fixed issue with bold
    phrases like "that saving throw" were being bolded due to an incorrect
    regex now only the words constitution, charisma etc before saving throw
    will be bolded

--- main.py
+++ main.py
@@ -197,10 +197,10 @@ def buildMarkdown(data: dict) -> str:
 		for d in desc:
 			d = re.sub(r"(\d*d\d+)", r"`dice:\g<1>`", d)
 			d = re.sub(r"(\w+ spell attack)", r"**\g<1>**", d)
-			d = re.sub(r"(\w+ saving throw)", r"**\g<1>**", d)
+			d = re.sub(r"((?:strength|dexterity|constitution|intelligence|wisdom|charisma) saving throw)", r"**\g<1>**", d, flags=re.IGNORECASE)
 			d = re.sub(r"(\d+ feet)", r"**\g<1>**", d)
 
-			d = re.sub(r"((?:Strength|Dexterity|Constitution|Intelligence|Wisdom|charisma)(?: \(.+\))? check)", r"**\g<1>**", d, flags=re.IGNORECASE)
+			d = re.sub(r"((?:strength|dexterity|constitution|intelligence|wisdom|charisma)(?: \(.+\))? check)", r"**\g<1>**", d, flags=re.IGNORECASE)
 			d = re.sub(r"(spell save DC)", r"**\g<1>**", d)
 
 			markdown += d+"\n\n"
